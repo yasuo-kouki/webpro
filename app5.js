f3e07db34b260b51de72fb597284472f5c82cdc9
yasuo-kouki <sidakou0804@gamil.com>
2024-11-04 15:13:59 +0900

    app5.jsにjankenを追加した

--- app5.js
+++ app5.js
@@ -29,27 +29,42 @@ app.get("/luck", (req, res) => {
 
 app.get("/janken", (req, res) => {
   let hand = req.query.hand;
-  let win = Number( req.query.win );
-  let total = Number( req.query.total );
-  console.log( {hand, win, total});
-  const num = Math.floor( Math.random() * 3 + 1 );
+  let win = Number(req.query.win);
+  let total = Number(req.query.total);
+  console.log({ hand, win, total }); // 確認のために表示
+  const num = Math.floor(Math.random() * 3 + 1); // 乱数表示
+
   let cpu = '';
-  if( num==1 ) cpu = 'グー';
-  else if( num==2 ) cpu = 'チョキ';
+  if (num === 1) cpu = 'グー';
+  else if (num === 2) cpu = 'チョキ';
   else cpu = 'パー';
-  // ここに勝敗の判定を入れる
-  // 今はダミーで人間の勝ちにしておく
-  let judgement = '勝ち';
-  win += 1;
-  total += 1;
+
+  // 勝敗の判定を行う
+  let judgement = '';
+  if (
+    (hand === 'グー' && cpu === 'チョキ') ||
+    (hand === 'チョキ' && cpu === 'パー') ||
+    (hand === 'パー' && cpu === 'グー')
+  ) {
+    judgement = '勝ち';
+    win += 1; // 勝った場合のみ増加
+  } else if (hand === cpu) {
+    judgement = 'あいこ';
+  } else {
+    judgement = '負け';
+  }
+
+  total += 1; // トータルは毎回増加
+
   const display = {
     your: hand,
     cpu: cpu,
     judgement: judgement,
     win: win,
-    total: total
-  }
-  res.render( 'janken', display );
+    total: total,
+  };
+  res.render('janken', display);
 });
+
 
 app.listen(8080, () => console.log("Example app listening on port 8080!"));
